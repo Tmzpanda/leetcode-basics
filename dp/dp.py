08284637378fc08055b7897e49380554da986ad8
Tim Zhang <tmzpanda@gmail.com>
2021-02-24 17:07:26 -0500

    Create dp.py

---
+++ dp/dp.py
@@ -0,0 +1,26 @@
+
+ 
+M1 - time complexity
+
+M2 - dp
+if possible -> "or"
+# 416. Partiton Equal Subset Sum - if possible - dp O(n*S)
+
+number of solutions -> "+"
+# 518. Coin Change - number of solutions - dp O(n*S)
+
+fewest -> "min()"
+# 300. Longest Increasing Subsequence - dp O(n^2)
+# 322. Coin Change - fewest coins - dp O(S*n)
+
+
+
+M3 - return
+dp[n]
+# 198. House Robber - dp O(n)
+# 322. Coin Change - fewest coins - dp O(S*n)
+# 188. Sell Stock - at most K transactions - dp O(k*n)
+
+max(dp)
+# 300. Longest Increasing Subsequence - dp O(n^2)
+# 221. Maximal Square - dp O(n^2)
